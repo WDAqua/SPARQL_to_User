f295e9fffcff1efc90515c8bc0827c7a1d819751
Dr Yous <dryous@wifiroam059148.univ-st-etienne.fr>
2017-06-29 17:47:52 +0200

    refactoring knowledge bases 2

--- src/main/java/eu/wdaqua/SPARQLtoUser/WebController.java
+++ src/main/java/eu/wdaqua/SPARQLtoUser/WebController.java
@@ -13,17 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebController {
     private static final Logger logger = LoggerFactory.getLogger(WebController.class);
-
      //Set this to allow browser requests from other websites
     @ModelAttribute
     public void setVaryResponseHeader(HttpServletResponse response) {
         response.setHeader("Access-Control-Allow-Origin", "*");
     }
     @RequestMapping("/sparqltouser")
-    public SPARQLToUser sparqlToUser(@RequestParam(value="sparql", defaultValue = "SELECT DISTINCT ?uri WHERE { <http://www.wikidata.org/entity/Q937> <http://www.wikidata.org/prop/direct/P184> ?uri}") String sparql,
+    public SPARQLToUser sparqlToUser(@RequestParam(value="sparql", defaultValue = "ASK WHERE { ?x <http://www.wikidata.org/prop/direct/P6> <http://www.wikidata.org/entity/Q76> . }") String sparql,
                                      @RequestParam(value="lang", defaultValue = "en") String lang,
                                      @RequestParam(value="kb", defaultValue = "wikidata") String kb,
-                                     @RequestParam(value="endpoint") String endpoint) {
+                                     @RequestParam(value="endpoint", defaultValue = "https://query.wikidata.org/sparql") String endpoint) {
             if (kb.contains("wikidata")){
                 endpoint = "https://query.wikidata.org/sparql";
             }else if (kb.contains("dbpedia")){
